d21333b53a5680ad50566471d81bc0a45abb7251
Bob Costas <finkzane@gmail.com>
2018-03-15 09:52:45 -0700

    Added set_color and set_parse_command to set color and parse command for parse script

--- split_blast.py
+++ split_blast.py
@@ -5,18 +5,39 @@ from Bio.Blast import NCBIXML
 
 
 class BlastInfo:
+
+    # class level member for the script that parses output
+    parse_file = 'sub_blast_parse.py'
+
     ''' Class used to store info needed to blast and parse
     '''
     def __init__( self, options, this_out, in_file, cmd ):
         # prefix to be prepended to parsed file names   
-        file_prefix = '.'.join( in_file.split('.')[ :-1 ] )
-        self.reg_out = '%s_parsed.txt' % ( file_prefix )
-        # TODO determine if this is number_hits?
-        self.no_hits
-        self.blast_cmd = cmd
+        self._file_prefix = '.'.join( in_file.split('.')[ :-1 ] )
 
+        # reg output for program output
+        self._reg_out = '%s_parsed.txt' % ( file_prefix )
+        # TODO determine if this is number_hits?
+        self._no_hits
+
+        # Command to be used for blasting
+        self._blast_cmd = cmd
+
+        set_parse_command()
         if opts.withColor:
             set_color()
-            parse_command()
+            self._parse_cmd += "--color_out %s " % ( self.color_out )
         
         
+    def set_color():
+        ''' Method to set the color of program output
+        '''
+        self.color_out = '%s_parsed_colored.txt' % ( self._file_prefix )
+
+    def set_parse_command():
+        self._parse_cmd = ( "%s --reg_out %s --no_hits %s " 
+                          "--numHits%d --numHsps %d --goodHit %s --xml %s"
+                            % ( parse_file, self._reg_out, self._no_hits, options.numHits, \
+                                options.numHsps, opts.goodHit, this_out )
+                                
+                          )
