1be64b771aee651cd3866fe7aeffd685d8f560b6
Bob Costas <finkzane@gmail.com>
2018-03-15 15:13:37 -0700

    Added call to main, data structure to hold default values of options to remove magic numbers

--- split_blast.py
+++ split_blast.py
@@ -52,14 +52,15 @@ def main():
     options, arguments = option_parser.parse_args() 
 
 def add_options( parser_object ):
+    option_values = { 'numProcs': 4 }
     # Input/Output file options
-    option_parser.add_option( '-q', '--query', help = ( "Fasta query file. Can be a "
+    parser_object.add_option( '-q', '--query', help = ( "Fasta query file. Can be a "
                                                         "comma specified list of fastas "
                                                         "also. [None, Required]"
                                                       )
                             )
 
-    option_parser.add_option( '--ns', '--nucSubject', help = ( "Fasta file of "
+    parser_object.add_option( '--ns', '--nucSubject', help = ( "Fasta file of "
                                                                "nucleotide sequences to "
                                                                " compare the query sequences "
                                                                "to. Will format if necessary. "
@@ -67,7 +68,7 @@ def add_options( parser_object ):
                                                               )
                              )
 
-    option_parser.add_option( '--ps', '--protSubject', help = ( "Fasta file of "
+    parser_object.add_option( '--ps', '--protSubject', help = ( "Fasta file of "
                                                                 "protein sequences to compare "
                                                                 "the query sequences to. "
                                                                 "Will format if necessary. "
@@ -75,7 +76,7 @@ def add_options( parser_object ):
                                                                )
                             )
 
-    option_parser.add_option( '--wc', '--withColor', default = False, \
+    parser_object.add_option( '--wc', '--withColor', default = False, \
                               action = "store_true", help = ( "Use this flag if you want "
                                                               "the colored version of the "
                                                               "parsed output to be produced."
@@ -83,11 +84,11 @@ def add_options( parser_object ):
                              )
                         
     # General
-    option_parser.add_option( '-n', '--numProcs', type = 'int', default = 4, \
-                              help = "Number of separate blasts to start [4]"
+    parser_object.add_option( '-n', '--numProcs', type = 'int', default = option_values['numProcs'], \
+                              help = "Number of separate blasts to start [%s]" % (option_values['numProcs'])
                             )
 
-    option_parser.add_option( '-t', '--temp', default = './temp', help = ( "Name for the "
+    parser_object.add_option( '-t', '--temp', default = './temp', help = ( "Name for the "
                                                                            "temporary working "
                                                                            "directory. Will be created "
                                                                            " at the beginning of the "
@@ -97,57 +98,57 @@ def add_options( parser_object ):
                                                                            "[/.temp]"
                                                                           )
                              )
-    option_parser.add_option( '-b', '--blastType', help = ( "Type of blast to run. Options "
+    parser_object.add_option( '-b', '--blastType', help = ( "Type of blast to run. Options "
                                                             "blastn, blastx, blastp, tblastx, "
                                                             "tblastn. [blastn or blastx or "
                                                             "blastn, blastx]"
                                                            )
                              )
 
-    option_parser.add_option( '--dontIndex', default = False, action = 'store_true', \
+    parser_object.add_option( '--dontIndex', default = False, action = 'store_true', \
                               help = ( "Use this flag if you don't want the sciprt to "
                                        "try and index the database. This is necessary for "
                                        "complex databases like nt and nr"
                                      )
                             )
 
-    option_parser.add_option( '--keepOut', default = False, action = 'store_true', \
+    parser_object.add_option( '--keepOut', default = False, action = 'store_true', \
                               help = ( "Use this flag if you don't want "
                                        "to delete the non-parsed blast files "
                                        "automatically if format not XML]"
                                      )
                             )
 
-    option_parser.add_option( '--blastFull', default = False, action = 'store_true', \
+    parser_object.add_option( '--blastFull', default = False, action = 'store_true', \
                               help = ( "Blast full query for each task "
                                        "[automatically used if out format not XML]"
                                      )
                             )
     # Blast options
-    option_parser.add_option( '--task', default = 'megablast, dc-megablast, blastn', \
+    parser_object.add_option( '--task', default = 'megablast, dc-megablast, blastn', \
                               help = ( "Type of blastn to run. Options are "
                                        "blastn, dc-megablast, megablast. "
                                        "[megablast, dc-megablast, blastn]"
                                      )
                             )
 
-    option_parser.add_option( '--evalue', default = '10', \
+    parser_object.add_option( '--evalue', default = '10', \
                               help = "Maximum evalue for hit to be recorded [10]"
                             )
 
-    option_parser.add_option( '-o', '--outFmt', type = 'int', default = 5, \
+    parser_object.add_option( '-o', '--outFmt', type = 'int', default = 5, \
                               help = ( "Integer specifying the number of blast hits "
                                        " to report per query. [5]"
                                      )  
                             ) 
 
-    option_parser.add_option( '--numHits', type = 'int', default = 5, \
+    parser_object.add_option( '--numHits', type = 'int', default = 5, \
                               help = ( "Integer specifying the number of blast "
                                        "hits to report per query. [5] "
                                      )
                             )
 
-    option_parser.add_option( '--numHsps', thype = 'int', default = 1, \
+    parser_object.add_option( '--numHsps', type = 'int', default = 1, \
                               help = ( "Integer specifying the number of "
                                        "alignments to report per query/subject "
                                        "pair. [1]"
@@ -155,20 +156,21 @@ def add_options( parser_object ):
                             )
 
     # Determine what maxes it to next blast stage
-    option_parser.add_option( '--goodHit', default = '0.05', \
+    parser_object.add_option( '--goodHit', default = '0.05', \
                               help = ( "Floating point number specifying "
                                        " the evalue necessary for "
                                        " a hit to be significant. [0.05]"
                                      )
                             )
 
-    option_parser.add_option( '--orfSize', type = 'int', default = 100, \
+    parser_object.add_option( '--orfSize', type = 'int', default = 100, \
                               help = ( "Integer specifying the minimum size for an "
                                        "open reading frame to be considered significant "
                                      )
                             )
 
-
+if __name__ == '__main__':
+    main()
                               
 
                                        
